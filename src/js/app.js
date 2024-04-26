f7ec0f3acf22fbbda5494dc6f2fa9fbbe0248842
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2024-04-26 01:31:57 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -23,7 +23,6 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
@@ -31,19 +30,25 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+                ${cover}
+              <img src="${variables.avatarURL}" class="photo" />
+              <h1>${variables.name || "Lucy"} ${variables.lastname ||
+    "Boilett"}</h1>
+              <h2>${variables.role || "Web Developer"}</h2>
+              <h3>${variables.city || ""} ${variables.country ||
+    "Miami, USA"}</h3>
+              <ul class="${variables.socialMediaPosition}">
+              ${variables.twitter &&
+                `<li><a href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>`}
+              ${variables.github &&
+                `<li><a href="https://github.com/"><i class="fa fa-github"></i></a></li>`}
+              ${variables.linkedin &&
+                `<li><a href="https://linkedin.com/"><i class="fa fa-linkedin"></i></a></li>`}
+              ${variables.instagram &&
+                `<li><a href="https://instagram.com/"><i class="fa fa-instagram"></i></a></li>`}
+              </ul>
+            </div>
+        `;
 }
 
 /**
@@ -51,41 +56,39 @@ function render(variables = {}) {
  */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
+    // if includeCover is true the algorithm should
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
+    // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "left",
     // social media usernames
-    twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    twitter: "null",
+    github: "null",
+    linkedin: "null",
+    instagram: "null",
+    name: "",
+    lastname: "",
+    role: "",
+    country: "",
+    city: ""
   };
-  render(window.variables); // render the card for the first time
-
+  render(window.variables);
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      const attribute = e.target.getAttribute("for");
       let values = {};
       values[attribute] =
-        this.value == "" || this.value == "null"
+        this.value == ""
           ? null
           : this.value == "true"
           ? true
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+      render(Object.assign(window.variables, values));
     });
   });
 };
